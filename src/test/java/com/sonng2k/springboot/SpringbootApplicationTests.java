3929ad49b6ab46661524b658c9a29cc52bbf0721
Son Nguyen <29955043+SonNg2k@users.noreply.github.com>
2023-09-08 17:29:16 +0700

    Spring Boot starter with Unit test

---
+++ src/test/java/com/sonng2k/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonng2k.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
