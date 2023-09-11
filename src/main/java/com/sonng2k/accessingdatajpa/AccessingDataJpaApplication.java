4e4f3c75078003e3ce1967624e0ecad523319805
Son Nguyen <29955043+SonNg2k@users.noreply.github.com>
2023-09-11 11:56:45 +0700

    Complete "Accessing Data with JPA"

---
+++ src/main/java/com/sonng2k/accessingdatajpa/AccessingDataJpaApplication.java
@@ -0,0 +1,64 @@
+package com.sonng2k.accessingdatajpa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+// To get output (to the console, in this example), you need to set up a logger. Then you need to set up
+// some data and use it to generate output.
+
+// The demo() method returns a CommandLineRunner bean that automatically runs the code when the application
+// launches.
+
+@SpringBootApplication
+public class AccessingDataJpaApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(AccessingDataJpaApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataJpaApplication.class);
+    }
+
+    // The CustomerRepository is first fetched from the Spring application context
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
+            // save a few customers
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'Brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Customer customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            // fetch an individual customer by ID
+            Customer customer = repository.findById(1L);
+            log.info("Customer found with findById(1L):");
+            log.info("--------------------------------");
+            log.info(customer.toString());
+            log.info("");
+
+            // find all customers whose last name is "Bauer"
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            repository.findByLastName("Bauer").forEach(bauer -> {
+                log.info(bauer.toString());
+            });
+            // for (Customer bauer : repository.findByLastName("Bauer")) {
+            //  log.info(bauer.toString());
+            // }
+            log.info("");
+        };
+    }
+
+}
---
+++ src/main/java/com/sonng2k/accessingdatajpa/Customer.java
@@ -0,0 +1,54 @@
+package com.sonng2k.accessingdatajpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+// The Customer objects are annotated as a JPA entity (. Because no @Table annotation exists, it is assumed
+// that this entity is mapped to a table named Customer.)
+
+// The default constructor exists only for the sake of JPA. You do not use it directly, so it is designated
+// as protected. The other constructor is the one you use to create instances of Customer to be saved to the
+// database.
+
+// The Customer object’s `id` property is annotated with @Id so that JPA recognizes it as the object’s ID.
+// The `id` property is also annotated with @GeneratedValue to indicate that the ID should be generated
+// automatically.
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer() {
+    }
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
