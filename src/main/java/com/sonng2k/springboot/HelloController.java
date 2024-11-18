3cab6ea8876271dbb759d936af905a6eb6f8ab11
Son Nguyen <29955043+SonNg2k@users.noreply.github.com>
2024-11-18 22:55:13 +0700

    Remove comments that are not necessary anymore for Spring beginners

--- src/main/java/com/sonng2k/springboot/HelloController.java
+++ src/main/java/com/sonng2k/springboot/HelloController.java
@@ -2,20 +2,6 @@ package com.sonng2k.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-// Create a web controller
-
-// @RestController tells Spring that this class is a Component of type RestController and capable of handling
-// HTTP requests.
-
-// @GetMapping marks a method as a handler method
-
-// The class is flagged as a @RestController, meaning it is ready for use by Spring MVC to handle web
-// requests.
-// @GetMapping maps / to the index() method. When invoked from a browser or by using curl on the command line,
-// the method returns pure text. That is because @RestController combines @Controller and @ResponseBody, two
-// annotations that result in web requests returning data rather than a view.
-
 @RestController
 public class HelloController {
 
@@ -23,5 +9,4 @@ public class HelloController {
     public String index() {
         return "Greetings from Spring Boot!";
     }
-
 }
--- src/main/java/com/sonng2k/springboot/SpringbootApplication.java
+++ src/main/java/com/sonng2k/springboot/SpringbootApplication.java
@@ -1,7 +1,5 @@
 package com.sonng2k.springboot;
 
-// Create an Application class
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -31,5 +29,4 @@ public class SpringbootApplication {
 
         };
     }
-
 }
